4a4321d3e2e8b029da866e1bc01bdc8089271d97
Alexander Wildhagenb <alex.wildhagen@web.de>
2023-03-03 13:07:23 +0100

    latest css changes, position of labels, fixing bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :inflatables, dependent: :destroy
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
